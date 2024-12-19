4f331f49d8bbcaefa4f778a97ce6d1b959f6e4fb
joel Ivongbe <joelivongbe@engrjoelivon.ht.home>
2024-12-18 22:11:21 -0500

    fixed the immigration service changed it to moving abroad

--- main/urls.py
+++ main/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     
     # Services page
     path('services/', views.services, name='services'),
-    path('services/immigration-consulting/', views.immigration_consulting, name='immigration_consulting'),
-   path('services/study-abroad/', views.study_abroad, name='study_abroad'),
-   path('services/work-visas/', views.work_visas, name='work_visas'),
+    path('services/moving-abroad/', views.moving_abroad, name='moving_abroad'),
+    path('services/study-abroad/', views.study_abroad, name='study_abroad'),
+    path('services/work-visas/', views.work_visas, name='work_visas'),
     
     # New paths for remaining services
     path('services/permanent-residency/', views.permanent_residency, name='permanent_residency'),
