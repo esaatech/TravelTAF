f36f57bf3763517d2e1c1510980ca10de0d6ed1c
joel Ivongbe <joelivongbe@engrjoelivon.ht.home>
2024-12-21 02:35:01 -0500

    finished the resume

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -6,6 +6,6 @@ app_name = 'resume_builder'
 urlpatterns = [
     path('', views.resume_home, name='home'),
     path('create-resume/', views.create_resume, name='create_resume'),
-    path('optimize/', views.optimize_resume, name='optimize'),
+    path('optimize/', views.optimize_resume, name='optimize_resume'),
     path('download/<int:resume_id>/', views.download_resume, name='download'),
 ]
