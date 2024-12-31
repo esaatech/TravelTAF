963432ea9d905e55b38ddfa5fce092206f88e021
joel Ivongbe <joelivongbe@engrjoelivon.ht.home>
2024-12-31 11:04:29 -0500

    added table in server

--- promotions/templatetags/promotion_tags.py
+++ promotions/templatetags/promotion_tags.py
@@ -14,3 +14,37 @@ def show_promotions_carousel():
     ).order_by('order')
     
     return {'promotions': active_promotions}
+
+@register.inclusion_tag('promotions/school_promotions.html')
+def show_school_promotions():
+    school_promotions = Promotion.objects.filter(
+        promotion_type='school',
+        is_active=True,
+        start_date__lte=timezone.now()
+    ).exclude(
+        end_date__lt=timezone.now()
+    ).order_by('order')
+    
+    return {'promotions': school_promotions}
+
+@register.inclusion_tag('promotions/promotion_wrapper.html')
+def show_promotions(promotion_type='general', style='basic'):
+    """
+    Display promotions with specified type and style
+    
+    Args:
+        promotion_type: Slug of the promotion type (general, school, service)
+        style: Template style to use (basic, card, featured)
+    """
+    promotions = Promotion.objects.filter(
+        promotion_type__slug=promotion_type,
+        is_active=True,
+        start_date__lte=timezone.now()
+    ).exclude(
+        end_date__lt=timezone.now()
+    ).order_by('order')
+    
+    return {
+        'promotions': promotions,
+        'style': style
+    }
