d13dc8e0e55ca96ad41aad3e2cd1c9f12c1ea0c2
joel Ivongbe <joelivongbe@engrjoelivon.ht.home>
2025-01-02 16:05:09 -0500

    added the backend

--- customerSupport/urls.py
+++ customerSupport/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('submit-study-abroad/', views.submit_study_abroad_form, name='submit_study_abroad'),
     path('submit-moving-abroad/', views.submit_moving_abroad_form, name='submit_moving_abroad'),
+    path('api/register/', views.register_user, name='register_user'),
 ]
